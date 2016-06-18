d872b8796e5eab211868f8c3674835d146d46e6d
Gleb <git@gleb.nl>
2016-06-18 17:52:51 +0200

    Fixed the back button size and position calculations.

--- calendar-heatmap.js
+++ calendar-heatmap.js
@@ -890,12 +890,12 @@ angular.module('g1b.calendar-heatmap', []).
               scope.drawChart();
             });
           button.append('circle')
-            .attr('cx', label_padding / 2.75)
+            .attr('cx', label_padding / 3)
             .attr('cy', label_padding / 2.5)
-            .attr('r', circle_radius);
+            .attr('r', item_size / 2.5);
           button.append('text')
-            .attr('x', label_padding / 2.75)
-            .attr('y', label_padding / 2.5)
+            .attr('x', label_padding / 3)
+            .attr('y', label_padding / 3)
             .attr('dy', function () {
               return Math.floor(width / 100) / 3;
             })
