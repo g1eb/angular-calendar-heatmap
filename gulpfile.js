f82532e70e128f06786453f38f175abef331a145
Gleb <git@gleb.nl>
2016-12-10 13:16:17 +0100

    Added gulpfile for building purposes.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+ 
+gulp.task('build', ['js', 'css']);
+
+gulp.task('css', function () {
+  gulp.src('src/*.css')
+    .pipe(cssnano())
+    .pipe(concat('calendar-heatmap.min.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js', function () {
+  gulp.src('src/*.js')
+    .pipe(uglify())
+    .pipe(concat('calendar-heatmap.min.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.+(js|html|css)', ['build']);
+});
